95abbb00e4f183724db2e5e104391b5cf28a6873
TylerOlli <ollityler@gmail.com>
2017-02-26 21:08:52 -0500

    Created constructor for basic flashcard

---
+++ basic.js
@@ -0,0 +1,23 @@
+//Constructor for the basic flashcard
+var BasicFlashcard = function(front, back){
+	this.front = front;
+	this.back = back;
+}
+
+//Empty array that will house our basic flashcard objects
+var arr = [];
+
+var questions = {
+	"In what county does Charlotte reside": "Mecklenburg",
+	"What fast food chain was founded in Charlotte": "Bojangles",
+	"What major bank is located in Charlotte": "Bank of America",
+	"What is the name of our football team's mascot": "Sir Purr"
+};
+
+//Loop through material and construct objects
+for (var key in questions){
+	arr.push(new BasicFlashcard(key, questions[key]));
+};
+
+//Export the array of objects and the constructor out to flashcards.js
+module.exports = {arr, BasicFlashcard};
